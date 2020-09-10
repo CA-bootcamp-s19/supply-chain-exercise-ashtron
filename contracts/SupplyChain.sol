844ddcd70a86d50d125c8e2ea2c9753c58a925d1
ashtron <ashtonplaysted@gmail.com>
2020-09-10 20:38:59 +0200

    Minor refactoring

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -17,6 +17,7 @@ contract SupplyChain {
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+
   mapping (uint => Item) items;
 
   /* Add a line that creates an enum called State. This should have 4 states
@@ -90,9 +91,11 @@ contract SupplyChain {
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
-    emit LogForSale(skuCount);
     items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
     skuCount = skuCount + 1;
+
+    emit LogForSale(skuCount);
+
     return true;
   }
 
